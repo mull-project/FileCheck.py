f3ebab03619805207b70782741a23f9519403d48
Stanislav Pankevich <s.pankevich@gmail.com>
2019-11-05 21:51:38 +0100

    02_file_does_not_exist case

--- src/FileCheck.py
+++ src/FileCheck.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
 print(sys.argv[0])
 if len(sys.argv) == 1:
     print("<check-file> not specified")
+    exit(1)
+
+match_file = sys.argv[1]
+if not os.path.isfile(match_file):
+    print("Could not open check file '{}': No such file or directory".format(match_file))
     exit(1)
 
 for line in sys.stdin:
