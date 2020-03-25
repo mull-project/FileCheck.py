300c7e573b6cc53b45fbec0af0140a82980eb12d
Stanislav Pankevich <s.pankevich@gmail.com>
2020-03-25 23:04:21 +0100

    invoke: running LIT tests: LLVM and Py

---
+++ tasks.py
@@ -0,0 +1,52 @@
+import os
+import re
+
+from invoke import task
+
+FILECHECK_LLVM_8_EXEC = 'FileCheck-8.0.1'
+FILECHECK_LLVM_9_EXEC = 'FileCheck-9.0.1'
+
+
+def get_filecheck_py_path():
+    cwd = os.getcwd()
+    return "{cwd}/filecheck/FileCheck.py".format(cwd=cwd)
+
+
+def get_filecheck_llvm_path(filecheck_exec):
+    cwd = os.getcwd()
+    exec = "{cwd}/tests/integration/tools/FileCheck/{filecheck_exec}".format(cwd=cwd, filecheck_exec=filecheck_exec)
+    return exec
+
+
+def formatted_command(string):
+    return re.sub('\\s+', ' ', string).strip()
+
+
+def run_lit_tests(c, filecheck_exec, llvm_only):
+    assert c
+    assert filecheck_exec
+    assert llvm_only is not None
+
+    llvm_only_value = "1" if llvm_only else ""
+
+    cwd = os.getcwd()
+
+    command = formatted_command("""
+        CURRENT_DIR={cwd}
+        REAL_ONLY={llvm_only_value} \
+        FILECHECK_EXEC={filecheck_exec}
+        PATH={cwd}/tests/integration/tools/FileCheck:{cwd}/tests/integration/tools:$PATH
+        lit 
+        -vv 
+        {cwd}/tests/integration
+    """).format(cwd=cwd, filecheck_exec=filecheck_exec, llvm_only_value=llvm_only_value)
+
+    print(command)
+    c.run("{}".format(command))
+
+
+@task
+def test(c):
+    run_lit_tests(c, get_filecheck_llvm_path(FILECHECK_LLVM_8_EXEC), True)
+    run_lit_tests(c, get_filecheck_llvm_path(FILECHECK_LLVM_9_EXEC), True)
+    run_lit_tests(c, get_filecheck_py_path(), False)
